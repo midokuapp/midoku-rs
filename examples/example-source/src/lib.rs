b91e47911b72102b92751526fff7a4a5c4f53a7d
sehnryr <youn@melois.dev>
2024-04-28 21:39:16 +0200

    docs: make a better source implementation example

--- examples/example-source/src/lib.rs
+++ examples/example-source/src/lib.rs
@@ -2,11 +2,37 @@
 mod bindings;
 
 use bindings::exports::midoku::example_source::api::{Chapter, Guest, Manga, Page};
+use bindings::midoku::http::outgoing_handler::{handle, IncomingResponse, Method};
+use bindings::midoku::limiter::rate_limiter::{block, set_burst, set_period_ms};
+
+const URL: &str = "http://example.com";
 
 struct Component;
 
 impl Guest for Component {
+    fn initialize() -> Result<(), ()> {
+        // Set the rate limiter to 3 requests per second
+        set_burst(3)?;
+        set_period_ms(1000)?;
+
+        Ok(())
+    }
+
     fn get_manga_list(_page: u32) -> Result<(Vec<Manga>, bool), ()> {
+        // Block until the rate limiter allows the request
+        block();
+
+        // Send a GET request to the API
+        let url = [URL, "/manga"].join("");
+        let response: IncomingResponse = handle(Method::Get, &url, None, None)?;
+
+        // Get the response bytes
+        let _bytes: Vec<u8> = response.bytes()?;
+
+        // Parse the response bytes into a list of manga with serde or similar
+        // library. This is just a placeholder to show how to use the response.
+        // The actual implementation will depend on the API.
+
         Err(()) // unimplemented
     }
 
