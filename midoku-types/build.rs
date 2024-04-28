675d886fa037e551f19c507fde0fa1cc57ad006d
sehnryr <youn@melois.dev>
2024-04-28 21:32:54 +0200

    feat: move generated types to "witgen" folder
    it would have more sense to have it named "wit" but `cargo-component` automatically creates bindings whether the crate is a wasm component or not

--- midoku-types/build.rs
+++ midoku-types/build.rs
@@ -11,11 +11,12 @@ fn main() -> Result<()> {
     let mut buffer = String::new();
 
     buffer.push_str("// auto-generated file by the build script from midoku-types\n");
-    buffer.push_str("package midoku:types;\n\ninterface types {\n");
+    buffer.push_str(format!("package midoku:types@{};", env!("CARGO_PKG_VERSION")).as_str());
+    buffer.push_str("\n\ninterface types {\n");
     buffer.push_str(&Witgen::gen_static_from_path(&PathBuf::from(""))?);
     buffer.push_str("}\n\nworld prelude {\n    export types;\n}\n");
 
-    let mut file = File::create("../wit/types.wit")?;
+    let mut file = File::create("witgen/types.wit")?;
     file.write(buffer.as_bytes())?;
 
     Ok(())
