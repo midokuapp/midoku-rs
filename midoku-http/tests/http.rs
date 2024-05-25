b969bbca52dc0f57aec4c6cdf43e97ea0d9c9bb3
sehnryr <youn@melois.dev>
2024-05-25 14:24:08 +0200

    test: add integration test for midoku-http crate

---
+++ midoku-http/tests/http.rs
@@ -0,0 +1,23 @@
+use midoku_http;
+
+#[test]
+fn test_handle_response_body() {
+    let response = midoku_http::outgoing_handler::handle(
+        midoku_http::types::Method::Get,
+        "https://jsonplaceholder.typicode.com/todos/1".to_string(),
+        None,
+        None,
+    );
+    assert!(response.is_ok());
+    let response = response.unwrap();
+    let bytes = response.bytes();
+
+    let expected = r#"{
+  "userId": 1,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+}"#;
+    let actual = std::str::from_utf8(bytes).unwrap();
+    assert_eq!(actual, expected);
+}
