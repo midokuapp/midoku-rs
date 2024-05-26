c6ecf54a67f0e4433bdae32a7242ee7d083c13a4
sehnryr <youn@melois.dev>
2024-05-26 04:01:59 +0200

    docs: add better documentation through example extension
    and change return values to Ok(T)

--- examples/example-extension/src/lib.rs
+++ examples/example-extension/src/lib.rs
@@ -1,7 +1,11 @@
 #[allow(warnings)]
 mod bindings;
 
-use bindings::exports::midoku::bindings::api::{Chapter, Filter, Guest, Manga, Page};
+use bindings::exports::midoku::bindings::api::Guest;
+use bindings::exports::midoku::types::chapter::Chapter;
+use bindings::exports::midoku::types::filter::Filter;
+use bindings::exports::midoku::types::manga::{ContentRating, Manga, ReadingMode, Status};
+use bindings::exports::midoku::types::page::Page;
 use bindings::midoku::http::outgoing_handler::{handle, IncomingResponse, Method};
 use bindings::midoku::limiter::rate_limiter::{block, set_burst, set_period_ms};
 
@@ -11,7 +15,10 @@ struct Component;
 
 impl Guest for Component {
     fn initialize() -> Result<(), ()> {
-        // Set the rate limiter to 3 requests per second
+        // The actual rate limiter configuration will depend on the API.
+
+        // For example, to configure a rate limiter to allow 3 requests per
+        // second:
         set_burst(3)?;
         set_period_ms(1000)?;
 
@@ -33,19 +40,56 @@ impl Guest for Component {
         // library. This is just a placeholder to show how to use the response.
         // The actual implementation will depend on the API.
 
-        Err(()) // unimplemented
+        // Example result
+        Ok((vec![], false))
     }
 
     fn get_manga_details(_manga_id: String) -> Result<Manga, ()> {
-        Err(()) // unimplemented
+        // You might want to block until the rate limiter allows the request
+        // block();
+
+        // This is where you would send a request to get the manga details,
+        // parse the response, and return the details. The actual implementation
+        // will depend on the API.
+
+        // Example result
+        Ok(Manga {
+            id: "".to_string(),
+            title: "".to_string(),
+            url: "".to_string(),
+            description: "".to_string(),
+            cover_url: "".to_string(),
+            author_name: "".to_string(),
+            artist_name: "".to_string(),
+            categories: vec![],
+            status: Status::Unknown,
+            content_rating: ContentRating::Safe,
+            reading_mode: ReadingMode::RightToLeft,
+        })
     }
 
     fn get_chapter_list(_manga_id: String) -> Result<Vec<Chapter>, ()> {
-        Err(()) // unimplemented
+        // You might want to block until the rate limiter allows the request
+        // block();
+
+        // This is where you would send a request to get the list of chapters,
+        // parse the response, and return the list of chapters. The actual
+        // implementation will depend on the API.
+
+        // Example result
+        Ok(vec![])
     }
 
     fn get_page_list(_manga_id: String, _chapter_id: String) -> Result<Vec<Page>, ()> {
-        Err(()) // unimplemented
+        // You might want to block until the rate limiter allows the request
+        // block();
+
+        // This is where you would send a request to get the list of pages,
+        // parse the response, and return the list of pages. The actual
+        // implementation will depend on the API.
+
+        // Example result
+        Ok(vec![])
     }
 }
 
